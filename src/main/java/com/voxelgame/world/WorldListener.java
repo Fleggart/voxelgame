124ebaf49176847c0ec72826e9309539c3d24cc4
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-06-04 10:09:44 +0800

    Create WorldListener.java

---
+++ src/main/java/com/voxelgame/world/WorldListener.java
@@ -0,0 +1,9 @@
+package com.voxelengine.world;
+
+public interface WorldListener {
+   void blockChanged(int x, int y, int z);
+
+   void lightColumnChanged(int x, int z, int y0, int y1);
+
+   void allChanged();
+}

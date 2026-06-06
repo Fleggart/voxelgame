fabfcd545b4d6154e430bdec148c79a076183630
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-06-07 06:34:37 +0800

    Update Tile.java

--- src/main/java/com/voxelgame/level/Tile.java
+++ src/main/java/com/voxelgame/level/Tile.java
@@ -1,96 +1,147 @@
 package com.voxelgame.level;
 
-import org.lwjgl.BufferUtils;
-import java.nio.FloatBuffer;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL15.*;
-import static org.lwjgl.opengl.GL20.*;
-import static org.lwjgl.opengl.GL30.*;
-
-public class Tesselator {
-    private static final int MAX_VERTICES = 100000;
-    private FloatBuffer vertexBuffer = BufferUtils.createFloatBuffer(MAX_VERTICES * 3);
-    private FloatBuffer texCoordBuffer = BufferUtils.createFloatBuffer(MAX_VERTICES * 2);
-    private FloatBuffer colorBuffer = BufferUtils.createFloatBuffer(MAX_VERTICES * 3);
-    private int vertices = 0;
-    private float u, v;
-    private float r, g, b;
-    private boolean hasColor = false;
-    private boolean hasTexture = false;
+public class Tile {
+    public static Tile rock = new Tile(0);
+    public static Tile grass = new Tile(1);
+    private int tex;
     
-    public void flush() {
-        vertexBuffer.flip();
-        texCoordBuffer.flip();
-        colorBuffer.flip();
+    private Tile(int tex) {
+        this.tex = tex;
+    }
+    
+    public void render(Tesselator t, Level level, int layer, int x, int y, int z) {
+        float u0 = tex / 16.0f;
+        float u1 = u0 + 0.0624375f;
+        float v0 = 0.0f;
+        float v1 = v0 + 0.0624375f;
+        float c1 = 1.0f;
+        float c2 = 0.8f;
+        float c3 = 0.6f;
+        float x0 = x;
+        float x1 = x + 1;
+        float y0 = y;
+        float y1 = y + 1;
+        float z0 = z;
+        float z1 = z + 1;
         
-        glEnableClientState(GL_VERTEX_ARRAY);
-        glVertexPointer(3, GL_FLOAT, 0, vertexBuffer);
-        
-        if (hasTexture) {
-            glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-            glTexCoordPointer(2, GL_FLOAT, 0, texCoordBuffer);
+        // 底面 (Y-)
+        if (!level.isSolidTile(x, y - 1, z)) {
+            float br = level.getBrightness(x, y - 1, z) * c1;
+            if ((br == c1) != (layer == 1)) {
+                t.color(br, br, br);
+                t.tex(u0, v1); t.vertex(x0, y0, z1);
+                t.tex(u0, v0); t.vertex(x0, y0, z0);
+                t.tex(u1, v0); t.vertex(x1, y0, z0);
+                t.tex(u1, v1); t.vertex(x1, y0, z1);
+            }
         }
         
-        if (hasColor) {
-            glEnableClientState(GL_COLOR_ARRAY);
-            glColorPointer(3, GL_FLOAT, 0, colorBuffer);
+        // 顶面 (Y+)
+        if (!level.isSolidTile(x, y + 1, z)) {
+            float br = level.getBrightness(x, y, z) * c1;
+            if ((br == c1) != (layer == 1)) {
+                t.color(br, br, br);
+                t.tex(u1, v1); t.vertex(x1, y1, z1);
+                t.tex(u1, v0); t.vertex(x1, y1, z0);
+                t.tex(u0, v0); t.vertex(x0, y1, z0);
+                t.tex(u0, v1); t.vertex(x0, y1, z1);
+            }
         }
         
-        glDrawArrays(GL_QUADS, 0, vertices);
-        
-        glDisableClientState(GL_VERTEX_ARRAY);
-        if (hasTexture) glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-        if (hasColor) glDisableClientState(GL_COLOR_ARRAY);
-        
-        clear();
-    }
-    
-    private void clear() {
-        vertices = 0;
-        vertexBuffer.clear();
-        texCoordBuffer.clear();
-        colorBuffer.clear();
-    }
-    
-    public void init() {
-        clear();
-        hasColor = false;
-        hasTexture = false;
-    }
-    
-    public void tex(float u, float v) {
-        hasTexture = true;
-        this.u = u;
-        this.v = v;
-    }
-    
-    public void color(float r, float g, float b) {
-        hasColor = true;
-        this.r = r;
-        this.g = g;
-        this.b = b;
-    }
-    
-    public void vertex(float x, float y, float z) {
-        vertexBuffer.put(vertices * 3, x);
-        vertexBuffer.put(vertices * 3 + 1, y);
-        vertexBuffer.put(vertices * 3 + 2, z);
-        
-        if (hasTexture) {
-            texCoordBuffer.put(vertices * 2, u);
-            texCoordBuffer.put(vertices * 2 + 1, v);
+        // 背面 (Z-)
+        if (!level.isSolidTile(x, y, z - 1)) {
+            float br = level.getBrightness(x, y, z - 1) * c2;
+            if ((br == c2) != (layer == 1)) {
+                t.color(br, br, br);
+                t.tex(u1, v0); t.vertex(x0, y1, z0);
+                t.tex(u0, v0); t.vertex(x1, y1, z0);
+                t.tex(u0, v1); t.vertex(x1, y0, z0);
+                t.tex(u1, v1); t.vertex(x0, y0, z0);
+            }
         }
         
-        if (hasColor) {
-            colorBuffer.put(vertices * 3, r);
-            colorBuffer.put(vertices * 3 + 1, g);
-            colorBuffer.put(vertices * 3 + 2, b);
+        // 正面 (Z+)
+        if (!level.isSolidTile(x, y, z + 1)) {
+            float br = level.getBrightness(x, y, z + 1) * c2;
+            if ((br == c2) != (layer == 1)) {
+                t.color(br, br, br);
+                t.tex(u0, v0); t.vertex(x0, y1, z1);
+                t.tex(u0, v1); t.vertex(x0, y0, z1);
+                t.tex(u1, v1); t.vertex(x1, y0, z1);
+                t.tex(u1, v0); t.vertex(x1, y1, z1);
+            }
         }
         
-        vertices++;
+        // 左面 (X-)
+        if (!level.isSolidTile(x - 1, y, z)) {
+            float br = level.getBrightness(x - 1, y, z) * c3;
+            if ((br == c3) != (layer == 1)) {
+                t.color(br, br, br);
+                t.tex(u1, v0); t.vertex(x0, y1, z1);
+                t.tex(u0, v0); t.vertex(x0, y1, z0);
+                t.tex(u0, v1); t.vertex(x0, y0, z0);
+                t.tex(u1, v1); t.vertex(x0, y0, z1);
+            }
+        }
         
-        if (vertices >= MAX_VERTICES - 4) {
-            flush();
+        // 右面 (X+)
+        if (!level.isSolidTile(x + 1, y, z)) {
+            float br = level.getBrightness(x + 1, y, z) * c3;
+            if ((br == c3) != (layer == 1)) {
+                t.color(br, br, br);
+                t.tex(u0, v1); t.vertex(x1, y0, z1);
+                t.tex(u1, v1); t.vertex(x1, y0, z0);
+                t.tex(u1, v0); t.vertex(x1, y1, z0);
+                t.tex(u0, v0); t.vertex(x1, y1, z1);
+            }
+        }
+    }
+    
+    public void renderFace(Tesselator t, int x, int y, int z, int face) {
+        float x0 = x;
+        float x1 = x + 1;
+        float y0 = y;
+        float y1 = y + 1;
+        float z0 = z;
+        float z1 = z + 1;
+        
+        switch (face) {
+            case 0: // 底面
+                t.vertex(x0, y0, z1);
+                t.vertex(x0, y0, z0);
+                t.vertex(x1, y0, z0);
+                t.vertex(x1, y0, z1);
+                break;
+            case 1: // 顶面
+                t.vertex(x1, y1, z1);
+                t.vertex(x1, y1, z0);
+                t.vertex(x0, y1, z0);
+                t.vertex(x0, y1, z1);
+                break;
+            case 2: // 背面
+                t.vertex(x0, y1, z0);
+                t.vertex(x1, y1, z0);
+                t.vertex(x1, y0, z0);
+                t.vertex(x0, y0, z0);
+                break;
+            case 3: // 正面
+                t.vertex(x0, y1, z1);
+                t.vertex(x0, y0, z1);
+                t.vertex(x1, y0, z1);
+                t.vertex(x1, y1, z1);
+                break;
+            case 4: // 左面
+                t.vertex(x0, y1, z1);
+                t.vertex(x0, y1, z0);
+                t.vertex(x0, y0, z0);
+                t.vertex(x0, y0, z1);
+                break;
+            case 5: // 右面
+                t.vertex(x1, y0, z1);
+                t.vertex(x1, y0, z0);
+                t.vertex(x1, y1, z0);
+                t.vertex(x1, y1, z1);
+                break;
         }
     }
 }

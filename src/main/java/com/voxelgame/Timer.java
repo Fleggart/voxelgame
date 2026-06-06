799303cb69b5b6c49cdd8f2eea74b2134e399ac5
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-06-07 06:26:28 +0800

    Update Timer.java

--- src/main/java/com/voxelgame/Timer.java
+++ src/main/java/com/voxelgame/Timer.java
@@ -1,17 +1,17 @@
 package com.voxelgame;
 
 public class Timer {
-    private static final long NS_PER_SECOND = 1_000_000_000L;
-    private static final long MAX_NS_PER_UPDATE = 1_000_000_000L;
+    private static final long NS_PER_SECOND = 1000000000L;
+    private static final long MAX_NS_PER_UPDATE = 1000000000L;
     private static final int MAX_TICKS_PER_UPDATE = 100;
     
-    private final float ticksPerSecond;
+    private float ticksPerSecond;
     private long lastTime;
     public int ticks;
-    public float partialTick;
-    public float timeScale = 1.0F;
-    public float fps = 0.0F;
-    public float passedTime = 0.0F;
+    public float a;
+    public float timeScale = 1.0f;
+    public float fps = 0.0f;
+    public float passedTime = 0.0f;
     
     public Timer(float ticksPerSecond) {
         this.ticksPerSecond = ticksPerSecond;
@@ -23,22 +23,15 @@ public class Timer {
         long passedNs = now - lastTime;
         lastTime = now;
         
-        if (passedNs < 0) {
-            passedNs = 0;
-        }
-        if (passedNs > MAX_NS_PER_UPDATE) {
-            passedNs = MAX_NS_PER_UPDATE;
-        }
+        if (passedNs < 0) passedNs = 0;
+        if (passedNs > MAX_NS_PER_UPDATE) passedNs = MAX_NS_PER_UPDATE;
         
-        fps = NS_PER_SECOND / (float) passedNs;
-        passedTime += (float) passedNs * timeScale * ticksPerSecond / 1_000_000_000F;
+        fps = (float) (NS_PER_SECOND / passedNs);
+        passedTime += (float) passedNs * timeScale * ticksPerSecond / 1.0e9f;
         ticks = (int) passedTime;
+        if (ticks > MAX_TICKS_PER_UPDATE) ticks = MAX_TICKS_PER_UPDATE;
         
-        if (ticks > MAX_TICKS_PER_UPDATE) {
-            ticks = MAX_TICKS_PER_UPDATE;
-        }
-        
-        passedTime -= (float) ticks;
-        partialTick = passedTime;
+        passedTime -= ticks;
+        a = passedTime;
     }
 }

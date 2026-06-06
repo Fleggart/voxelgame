c20608340bb341e4cd402ad236400d42213d2780
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-06-06 14:22:55 +0800

    Delete src/main/java/com/voxelgame/world/WorldListener.java

--- src/main/java/com/voxelgame/world/WorldListener.java
+++
@@ -1,7 +0,0 @@
-package com.voxelgame.world;
-
-public interface WorldListener {
-   void blockChanged(int x, int y, int z);
-   void lightColumnChanged(int x, int z, int y0, int y1);
-   void allChanged();
-}

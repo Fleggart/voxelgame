fbde877d61beeba59f9089a69f8e08bbf85cdf2a
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-06-07 06:27:14 +0800

    Update HitResult.java

--- src/main/java/com/voxelgame/HitResult.java
+++ src/main/java/com/voxelgame/HitResult.java
@@ -4,14 +4,14 @@ public class HitResult {
     public int x;
     public int y;
     public int z;
-    public int side;
-    public int face;
+    public int o;
+    public int f;
     
-    public HitResult(int x, int y, int z, int side, int face) {
+    public HitResult(int x, int y, int z, int o, int f) {
         this.x = x;
         this.y = y;
         this.z = z;
-        this.side = side;
-        this.face = face;
+        this.o = o;
+        this.f = f;
     }
 }

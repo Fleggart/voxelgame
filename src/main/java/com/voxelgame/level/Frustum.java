9f91445fd5d4218aa1df02eee1d92b809ff0f388
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-06-07 06:36:47 +0800

    Update Frustum.java

--- src/main/java/com/voxelgame/level/Frustum.java
+++ src/main/java/com/voxelgame/level/Frustum.java
@@ -1,164 +1,123 @@
 package com.voxelgame.level;
 
-import com.voxelgame.physics.AABB;
-import java.nio.FloatBuffer;
+import com.voxelgame.phys.AABB;
+import org.joml.Matrix4f;
 import org.lwjgl.BufferUtils;
-
+import java.nio.FloatBuffer;
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL15.*;
-import static org.lwjgl.opengl.GL20.*;
 
 public class Frustum {
-    private final float[][] frustum = new float[6][4];
-    private static Frustum instance = new Frustum();
+    public float[][] m_Frustum = new float[6][4];
+    public static final int RIGHT = 0, LEFT = 1, BOTTOM = 2, TOP = 3, BACK = 4, FRONT = 5;
+    private static Frustum frustum = new Frustum();
+    private FloatBuffer _proj = BufferUtils.createFloatBuffer(16);
+    private FloatBuffer _modl = BufferUtils.createFloatBuffer(16);
+    private float[] proj = new float[16];
+    private float[] modl = new float[16];
+    private float[] clip = new float[16];
     
-    private final FloatBuffer projBuffer = BufferUtils.createFloatBuffer(16);
-    private final FloatBuffer modlBuffer = BufferUtils.createFloatBuffer(16);
-    private final float[] proj = new float[16];
-    private final float[] modl = new float[16];
-    private final float[] clip = new float[16];
-
     private Frustum() {}
-
-    public static Frustum getInstance() {
-        instance.calculateFrustum();
-        return instance;
+    
+    public static Frustum getFrustum() {
+        frustum.calculateFrustum();
+        return frustum;
     }
-
-    private void normalizePlane(int side) {
+    
+    private void normalizePlane(float[][] frustum, int side) {
         float magnitude = (float) Math.sqrt(
             frustum[side][0] * frustum[side][0] +
             frustum[side][1] * frustum[side][1] +
             frustum[side][2] * frustum[side][2]
         );
-        if (magnitude != 0) {
-            frustum[side][0] /= magnitude;
-            frustum[side][1] /= magnitude;
-            frustum[side][2] /= magnitude;
-            frustum[side][3] /= magnitude;
-        }
+        frustum[side][0] /= magnitude;
+        frustum[side][1] /= magnitude;
+        frustum[side][2] /= magnitude;
+        frustum[side][3] /= magnitude;
     }
-
+    
     private void calculateFrustum() {
-        projBuffer.clear();
-        modlBuffer.clear();
-        
-        // 获取投影矩阵和模型视图矩阵
-        glGetFloatv(GL_PROJECTION_MATRIX, projBuffer);
-        glGetFloatv(GL_MODELVIEW_MATRIX, modlBuffer);
-        
-        // 检查 buffer 是否有足够的数据
-        projBuffer.flip();
-        modlBuffer.flip();
-        
-        if (projBuffer.remaining() < 16 || modlBuffer.remaining() < 16) {
-            System.err.println("[DEBUG] Frustum: buffer underflow - proj=" + projBuffer.remaining() + ", modl=" + modlBuffer.remaining());
-            return;
-        }
-        
-        projBuffer.get(proj);
-        modlBuffer.get(modl);
+        _proj.clear();
+        _modl.clear();
+        glGetFloatv(GL_PROJECTION_MATRIX, _proj);
+        glGetFloatv(GL_MODELVIEW_MATRIX, _modl);
+        _proj.flip().limit(16);
+        _modl.flip().limit(16);
+        _proj.get(proj);
+        _modl.get(modl);
         
         // 计算裁剪矩阵
-        clip[0]  = modl[0] * proj[0] + modl[1] * proj[4] + modl[2] * proj[8] + modl[3] * proj[12];
-        clip[1]  = modl[0] * proj[1] + modl[1] * proj[5] + modl[2] * proj[9] + modl[3] * proj[13];
-        clip[2]  = modl[0] * proj[2] + modl[1] * proj[6] + modl[2] * proj[10] + modl[3] * proj[14];
-        clip[3]  = modl[0] * proj[3] + modl[1] * proj[7] + modl[2] * proj[11] + modl[3] * proj[15];
-        
-        clip[4]  = modl[4] * proj[0] + modl[5] * proj[4] + modl[6] * proj[8] + modl[7] * proj[12];
-        clip[5]  = modl[4] * proj[1] + modl[5] * proj[5] + modl[6] * proj[9] + modl[7] * proj[13];
-        clip[6]  = modl[4] * proj[2] + modl[5] * proj[6] + modl[6] * proj[10] + modl[7] * proj[14];
-        clip[7]  = modl[4] * proj[3] + modl[5] * proj[7] + modl[6] * proj[11] + modl[7] * proj[15];
-        
-        clip[8]  = modl[8] * proj[0] + modl[9] * proj[4] + modl[10] * proj[8] + modl[11] * proj[12];
-        clip[9]  = modl[8] * proj[1] + modl[9] * proj[5] + modl[10] * proj[9] + modl[11] * proj[13];
+        clip[0] = modl[0] * proj[0] + modl[1] * proj[4] + modl[2] * proj[8] + modl[3] * proj[12];
+        clip[1] = modl[0] * proj[1] + modl[1] * proj[5] + modl[2] * proj[9] + modl[3] * proj[13];
+        clip[2] = modl[0] * proj[2] + modl[1] * proj[6] + modl[2] * proj[10] + modl[3] * proj[14];
+        clip[3] = modl[0] * proj[3] + modl[1] * proj[7] + modl[2] * proj[11] + modl[3] * proj[15];
+        clip[4] = modl[4] * proj[0] + modl[5] * proj[4] + modl[6] * proj[8] + modl[7] * proj[12];
+        clip[5] = modl[4] * proj[1] + modl[5] * proj[5] + modl[6] * proj[9] + modl[7] * proj[13];
+        clip[6] = modl[4] * proj[2] + modl[5] * proj[6] + modl[6] * proj[10] + modl[7] * proj[14];
+        clip[7] = modl[4] * proj[3] + modl[5] * proj[7] + modl[6] * proj[11] + modl[7] * proj[15];
+        clip[8] = modl[8] * proj[0] + modl[9] * proj[4] + modl[10] * proj[8] + modl[11] * proj[12];
+        clip[9] = modl[8] * proj[1] + modl[9] * proj[5] + modl[10] * proj[9] + modl[11] * proj[13];
         clip[10] = modl[8] * proj[2] + modl[9] * proj[6] + modl[10] * proj[10] + modl[11] * proj[14];
         clip[11] = modl[8] * proj[3] + modl[9] * proj[7] + modl[10] * proj[11] + modl[11] * proj[15];
-        
         clip[12] = modl[12] * proj[0] + modl[13] * proj[4] + modl[14] * proj[8] + modl[15] * proj[12];
         clip[13] = modl[12] * proj[1] + modl[13] * proj[5] + modl[14] * proj[9] + modl[15] * proj[13];
         clip[14] = modl[12] * proj[2] + modl[13] * proj[6] + modl[14] * proj[10] + modl[15] * proj[14];
         clip[15] = modl[12] * proj[3] + modl[13] * proj[7] + modl[14] * proj[11] + modl[15] * proj[15];
         
-        // 提取平面
-        // 右平面
-        frustum[0][0] = clip[3] - clip[0];
-        frustum[0][1] = clip[7] - clip[4];
-        frustum[0][2] = clip[11] - clip[8];
-        frustum[0][3] = clip[15] - clip[12];
-        normalizePlane(0);
+        // 提取视锥平面
+        m_Frustum[LEFT][0] = clip[3] - clip[0];
+        m_Frustum[LEFT][1] = clip[7] - clip[4];
+        m_Frustum[LEFT][2] = clip[11] - clip[8];
+        m_Frustum[LEFT][3] = clip[15] - clip[12];
+        normalizePlane(m_Frustum, LEFT);
         
-        // 左平面
-        frustum[1][0] = clip[3] + clip[0];
-        frustum[1][1] = clip[7] + clip[4];
-        frustum[1][2] = clip[11] + clip[8];
-        frustum[1][3] = clip[15] + clip[12];
-        normalizePlane(1);
+        m_Frustum[RIGHT][0] = clip[3] + clip[0];
+        m_Frustum[RIGHT][1] = clip[7] + clip[4];
+        m_Frustum[RIGHT][2] = clip[11] + clip[8];
+        m_Frustum[RIGHT][3] = clip[15] + clip[12];
+        normalizePlane(m_Frustum, RIGHT);
         
-        // 底平面
-        frustum[2][0] = clip[3] + clip[1];
-        frustum[2][1] = clip[7] + clip[5];
-        frustum[2][2] = clip[11] + clip[9];
-        frustum[2][3] = clip[15] + clip[13];
-        normalizePlane(2);
+        m_Frustum[BOTTOM][0] = clip[3] + clip[1];
+        m_Frustum[BOTTOM][1] = clip[7] + clip[5];
+        m_Frustum[BOTTOM][2] = clip[11] + clip[9];
+        m_Frustum[BOTTOM][3] = clip[15] + clip[13];
+        normalizePlane(m_Frustum, BOTTOM);
         
-        // 顶平面
-        frustum[3][0] = clip[3] - clip[1];
-        frustum[3][1] = clip[7] - clip[5];
-        frustum[3][2] = clip[11] - clip[9];
-        frustum[3][3] = clip[15] - clip[13];
-        normalizePlane(3);
+        m_Frustum[TOP][0] = clip[3] - clip[1];
+        m_Frustum[TOP][1] = clip[7] - clip[5];
+        m_Frustum[TOP][2] = clip[11] - clip[9];
+        m_Frustum[TOP][3] = clip[15] - clip[13];
+        normalizePlane(m_Frustum, TOP);
         
-        // 远平面
-        frustum[4][0] = clip[3] - clip[2];
-        frustum[4][1] = clip[7] - clip[6];
-        frustum[4][2] = clip[11] - clip[10];
-        frustum[4][3] = clip[15] - clip[14];
-        normalizePlane(4);
+        m_Frustum[BACK][0] = clip[3] - clip[2];
+        m_Frustum[BACK][1] = clip[7] - clip[6];
+        m_Frustum[BACK][2] = clip[11] - clip[10];
+        m_Frustum[BACK][3] = clip[15] - clip[14];
+        normalizePlane(m_Frustum, BACK);
         
-        // 近平面
-        frustum[5][0] = clip[3] + clip[2];
-        frustum[5][1] = clip[7] + clip[6];
-        frustum[5][2] = clip[11] + clip[10];
-        frustum[5][3] = clip[15] + clip[14];
-        normalizePlane(5);
+        m_Frustum[FRONT][0] = clip[3] + clip[2];
+        m_Frustum[FRONT][1] = clip[7] + clip[6];
+        m_Frustum[FRONT][2] = clip[11] + clip[10];
+        m_Frustum[FRONT][3] = clip[15] + clip[14];
+        normalizePlane(m_Frustum, FRONT);
     }
-
-    public boolean pointInFrustum(float x, float y, float z) {
-        for (int i = 0; i < 6; i++) {
-            if (frustum[i][0] * x + frustum[i][1] * y + frustum[i][2] * z + frustum[i][3] <= 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public boolean sphereInFrustum(float x, float y, float z, float radius) {
-        for (int i = 0; i < 6; i++) {
-            if (frustum[i][0] * x + frustum[i][1] * y + frustum[i][2] * z + frustum[i][3] <= -radius) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public boolean cubeInFrustum(float x1, float y1, float z1, float x2, float y2, float z2) {
-        for (int i = 0; i < 6; i++) {
-            if (frustum[i][0] * x1 + frustum[i][1] * y1 + frustum[i][2] * z1 + frustum[i][3] <= 0 &&
-                frustum[i][0] * x2 + frustum[i][1] * y1 + frustum[i][2] * z1 + frustum[i][3] <= 0 &&
-                frustum[i][0] * x1 + frustum[i][1] * y2 + frustum[i][2] * z1 + frustum[i][3] <= 0 &&
-                frustum[i][0] * x2 + frustum[i][1] * y2 + frustum[i][2] * z1 + frustum[i][3] <= 0 &&
-                frustum[i][0] * x1 + frustum[i][1] * y1 + frustum[i][2] * z2 + frustum[i][3] <= 0 &&
-                frustum[i][0] * x2 + frustum[i][1] * y1 + frustum[i][2] * z2 + frustum[i][3] <= 0 &&
-                frustum[i][0] * x1 + frustum[i][1] * y2 + frustum[i][2] * z2 + frustum[i][3] <= 0 &&
-                frustum[i][0] * x2 + frustum[i][1] * y2 + frustum[i][2] * z2 + frustum[i][3] <= 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
+    
     public boolean cubeInFrustum(AABB aabb) {
         return cubeInFrustum(aabb.x0, aabb.y0, aabb.z0, aabb.x1, aabb.y1, aabb.z1);
+    }
+    
+    public boolean cubeInFrustum(float x1, float y1, float z1, float x2, float y2, float z2) {
+        for (int i = 0; i < 6; i++) {
+            if (m_Frustum[i][0] * x1 + m_Frustum[i][1] * y1 + m_Frustum[i][2] * z1 + m_Frustum[i][3] <= 0 &&
+                m_Frustum[i][0] * x2 + m_Frustum[i][1] * y1 + m_Frustum[i][2] * z1 + m_Frustum[i][3] <= 0 &&
+                m_Frustum[i][0] * x1 + m_Frustum[i][1] * y2 + m_Frustum[i][2] * z1 + m_Frustum[i][3] <= 0 &&
+                m_Frustum[i][0] * x2 + m_Frustum[i][1] * y2 + m_Frustum[i][2] * z1 + m_Frustum[i][3] <= 0 &&
+                m_Frustum[i][0] * x1 + m_Frustum[i][1] * y1 + m_Frustum[i][2] * z2 + m_Frustum[i][3] <= 0 &&
+                m_Frustum[i][0] * x2 + m_Frustum[i][1] * y1 + m_Frustum[i][2] * z2 + m_Frustum[i][3] <= 0 &&
+                m_Frustum[i][0] * x1 + m_Frustum[i][1] * y2 + m_Frustum[i][2] * z2 + m_Frustum[i][3] <= 0 &&
+                m_Frustum[i][0] * x2 + m_Frustum[i][1] * y2 + m_Frustum[i][2] * z2 + m_Frustum[i][3] <= 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }

3870a3a6093592989ef89d22a3960721db0f544f
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-06-07 11:24:42 +0800

    Refactor texture loading to use BufferedImage

--- src/main/java/com/voxelgame/Textures.java
+++ src/main/java/com/voxelgame/Textures.java
@@ -1,74 +1,59 @@
 package com.voxelgame;
 
-import org.lwjgl.BufferUtils;
-import org.lwjgl.stb.STBImage;
-import org.lwjgl.system.MemoryStack;
-
+import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 import java.util.HashMap;
-
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL30.*;
+import javax.imageio.ImageIO;
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.util.glu.GLU;
 
 public class Textures {
-    private static HashMap<String, Integer> idMap = new HashMap<>();
-    private static int lastId = -1;
-    
-    public static int loadTexture(String resourceName, int mode) {
-        if (idMap.containsKey(resourceName)) {
-            return idMap.get(resourceName);
-        }
-        
-        try {
-            // 从资源加载图片
-            ByteBuffer imageBuffer;
-            try (InputStream is = Textures.class.getResourceAsStream(resourceName)) {
-                if (is == null) {
-                    throw new RuntimeException("Texture not found: " + resourceName);
-                }
-                byte[] bytes = is.readAllBytes();
-                imageBuffer = BufferUtils.createByteBuffer(bytes.length);
-                imageBuffer.put(bytes);
-                imageBuffer.flip();
+   private static HashMap<String, Integer> idMap = new HashMap();
+   private static int lastId = -9999999;
+
+   public static int loadTexture(String resourceName, int mode) {
+      try {
+         if (idMap.containsKey(resourceName)) {
+            return (Integer)idMap.get(resourceName);
+         } else {
+            IntBuffer ib = BufferUtils.createIntBuffer(1);
+            GL11.glGenTextures(ib);
+            int id = ib.get(0);
+            bind(id);
+            GL11.glTexParameteri(3553, 10241, mode);
+            GL11.glTexParameteri(3553, 10240, mode);
+            BufferedImage img = ImageIO.read(Textures.class.getResourceAsStream(resourceName));
+            int w = img.getWidth();
+            int h = img.getHeight();
+            ByteBuffer pixels = BufferUtils.createByteBuffer(w * h * 4);
+            int[] rawPixels = new int[w * h];
+            img.getRGB(0, 0, w, h, rawPixels, 0, w);
+
+            for(int i = 0; i < rawPixels.length; ++i) {
+               int a = rawPixels[i] >> 24 & 255;
+               int r = rawPixels[i] >> 16 & 255;
+               int g = rawPixels[i] >> 8 & 255;
+               int b = rawPixels[i] & 255;
+               rawPixels[i] = a << 24 | b << 16 | g << 8 | r;
             }
-            
-            IntBuffer width = BufferUtils.createIntBuffer(1);
-            IntBuffer height = BufferUtils.createIntBuffer(1);
-            IntBuffer channels = BufferUtils.createIntBuffer(1);
-            
-            ByteBuffer pixels = STBImage.stbi_load_from_memory(imageBuffer, width, height, channels, 4);
-            if (pixels == null) {
-                throw new RuntimeException("Failed to load texture: " + STBImage.stbi_failure_reason());
-            }
-            
-            int id = glGenTextures();
-            glBindTexture(GL_TEXTURE_2D, id);
-            
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, mode);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, mode);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-            
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width.get(0), height.get(0), 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
-            glGenerateMipmap(GL_TEXTURE_2D);
-            
-            STBImage.stbi_image_free(pixels);
-            
-            idMap.put(resourceName, id);
+
+            pixels.asIntBuffer().put(rawPixels);
+            GLU.gluBuild2DMipmaps(3553, 6408, w, h, 6408, 5121, pixels);
             return id;
-            
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to load texture", e);
-        }
-    }
-    
-    public static void bind(int id) {
-        if (id != lastId) {
-            glBindTexture(GL_TEXTURE_2D, id);
-            lastId = id;
-        }
-    }
+         }
+      } catch (IOException var14) {
+         throw new RuntimeException("!!");
+      }
+   }
+
+   public static void bind(int id) {
+      if (id != lastId) {
+         GL11.glBindTexture(3553, id);
+         lastId = id;
+      }
+
+   }
 }
